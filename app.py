4d0fd93ea5883d090068d760afd52f15184b8a04
chie <n.cotori10@outlook.jp>
2024-10-08 14:15:27 +0900

    エラー解消

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # データベース接続関数
 def get_db_connection():
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect('food_stock07.db')
     conn.row_factory = sqlite3.Row
     return conn
 
